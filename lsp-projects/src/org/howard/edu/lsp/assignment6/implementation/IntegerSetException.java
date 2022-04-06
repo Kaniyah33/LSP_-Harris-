a41fcab5cf0fac2e541c624402c22a5e8d5c59f3
kaniyah <kaniyah@localhost>
2022-04-06 12:05:50 -0400

    Assignment 6 (Integer set code referenced from assignment 5)

---
+++ lsp-projects/src/org/howard/edu/lsp/assignment6/implementation/IntegerSetException.java
@@ -0,0 +1,13 @@
+package org.howard.edu.lsp.assignment6.implementation;
+
+public class IntegerSetException extends Exception {
+	
+	/**
+	 * Exception class for IntegerSet
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public IntegerSetException(String errormsg) {
+		super(errormsg);
+	}
+}
